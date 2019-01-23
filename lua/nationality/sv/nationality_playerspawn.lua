f0ae98a6021bf9f2e2cb0a252f59dcd2d1899a0f
BlayneWright <41925552+NettoSpaghetto@users.noreply.github.com>
2019-01-23 17:16:24 +0000

    removed preset spawnpoints

--- lua/nationality/sv/nationality_playerspawn.lua
+++ lua/nationality/sv/nationality_playerspawn.lua
@@ -1,21 +1,9 @@
 NATION_AM = 0
 NATION_MEX = 1
-local americanSpawn = {
-    Vector(-2178.396973, -852.222412, 46.445641),
-    Vector(-2035.395630, -852.278931, 45.450115),
-    Vector(-1898.510254, -852.332947, 47.652611),
-    Vector(-1898.443115, -679.083862, 48.716255),
-    Vector(-2042.164307, -679.027283, 48.226990),
-    Vector(-2200.868164, -678.964661, 46.589111),
-}
-local mexicanSpawn = {
-    Vector(385.474854, -1631.199463, 57.031250),
-    Vector(384.724579, -1568.852783, 57.031250),
-    Vector(383.935791, -1503.304688, 57.031250),
-    Vector(624.017761, -1500.590210, 57.031250),
-    Vector(624.592346, -1575.653076, 57.031250),
-    Vector(625.166992, -1650.713013, 57.031250),
-}
+local nation = {}
+
+local function SaveNationSpawns()
+end
 
 local function nationalitySpawn(ply)
     local userDataFile = "nationality/" ..  ply:SteamID64() .. ".txt" 
@@ -47,3 +35,16 @@ local function spawnOnRightSide(ply)
 end
 
 hook.Add("PlayerSpawn", "NationalitySpawnOnRightSide", spawnOnRightSide)
+
+--Handle saving and loading of slots
+hook.Add("PlayerSay", "HandleBDONCommands" , function(ply, text)
+	if string.sub(string.lower(text), 1, 12) == "/addspawnmex" then
+		if table.HasValue(BDON_CONFIG.AdminRanks, ply:GetUserGroup()) then
+			SaveDoubleOrNothingSlots()
+			ply:ChatPrint("Double Or Nothing Slot Machines have been saved for the map "..game.GetMap().."!")
+		else
+			ply:ChatPrint("You do not have permission to perform this action, please contact an admin.")
+		end
+	end
+end)
+
