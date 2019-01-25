19584f140de8ce68c00e6b1a92cc1835d200f8cc
BlayneWright <41925552+NettoSpaghetto@users.noreply.github.com>
2019-01-25 21:59:29 +0000

    Added custom spawns, they save and can be deleted

--- lua/nationality/sv/nationality_playerspawn.lua
+++ lua/nationality/sv/nationality_playerspawn.lua
@@ -4,20 +4,38 @@ nation = nation or {}
 nation.mex = nation.mex or {}
 nation.am = nation.am or {}
 
-local function addMexSpawn(ply)
-	table.insert(nation.mex, {pos = ply:GetPos()})
+local function addMexSpawn( ply )
+
+    table.insert(nation.mex, {pos = ply:GetPos()})
+    
 end
 
-local function addAmSpawn(ply)
-	table.insert(nation.am, {pos = ply:GetPos()})
+local function addAmSpawn( ply )
+
+    table.insert(nation.am, {pos = ply:GetPos()})
+    
 end
 
 local function saveSpawns()
-    file.Write("nationality/"..game.GetMap().."_mex.txt", util.TableToJSON(nation.mex))
-    file.Write("nationality/"..game.GetMap().."_am.txt", util.TableToJSON(nation.am))
+
+    file.Write( "nationality/" .. game.GetMap() .. "_mex.txt", util.TableToJSON( nation.mex ) )
+    file.Write( "nationality/" .. game.GetMap() .. "_am.txt", util.TableToJSON(nation.am))
+
 end
 
-local function nationalitySpawn(ply)
+local function deleteSpawns( ply )
+
+    if not file.Exists( "nationality/"..game.GetMap().."_mex.txt", "DATA" ) then 
+        return ply:ChatPrint( "No spawn points have been saved!" ) 
+    else
+        return ply:ChatPrint( "Spawn points for" .. game.GetMap() .. "deleted!" )
+
+    file.Delete( "nationality/" .. game.GetMap() .. "_mex.txt" )
+    file.Delete( "nationality/" .. game.GetMap() .. "_am.txt" )
+
+end
+
+local function nationalitySpawn( ply )
 
     if not file.Exists( "nationality/"..game.GetMap().."_mex.txt", "DATA" ) then return end
     if not file.Exists( "nationality/"..game.GetMap().."_am.txt", "DATA" ) then return end
@@ -35,9 +53,9 @@ local function nationalitySpawn(ply)
     if file.Exists( userDataFile, "DATA" ) then
         if ply:IsValid() and ply:IsPlayer() then
             if tonumber(userDataContents, 2)  == NATION_MEX then
-                timer.Simple(0.5, function() ply:SetPos( mexicanSpawn[math.random( #mexicanSpawn ) ].pos ) end )
+                timer.Simple( 0.5, function() ply:SetPos( mexicanSpawn[math.random( #mexicanSpawn ) ].pos ) end )
             else
-                timer.Simple(0.5, function() ply:SetPos( americanSpawn[ math.random( #americanSpawn ) ].pos ) end )
+                timer.Simple( 0.5, function() ply:SetPos( americanSpawn[ math.random( #americanSpawn ) ].pos ) end )
             end
         end
     end
@@ -45,46 +63,61 @@ end
 
 hook.Add("PlayerInitialSpawn", "NationalityCheck", nationalitySpawn)
 
-local function spawnOnRightSide(ply)
+local function spawnOnRightSide( ply )
+
     if ply:IsValid() then
         if ply:GetNWFloat("Nation", NATION_MEX) == NATION_MEX then
-            timer.Simple(0.2, function() ply:SetPos( mexicanSpawn[math.random(#mexicanSpawn)].pos ) end )
+            timer.Simple( 0.2, function() ply:SetPos( mexicanSpawn[math.random( #mexicanSpawn ) ].pos ) end )
         elseif ply:GetNWFloat("Nation", NATION_AM) == NATION_AM then
-            timer.Simple(0.2, function() ply:SetPos( americanSpawn[math.random(#americanSpawn)].pos ) end )
+            timer.Simple( 0.2, function() ply:SetPos( americanSpawn[math.random( #americanSpawn ) ].pos ) end )
         end
     end
+
 end
 
 hook.Add("PlayerSpawn", "NationalitySpawnOnRightSide", spawnOnRightSide)
 
 --Handle saving and loading of slots
-hook.Add("PlayerSay", "NationalityCommands" , function(ply, text)
-	if string.sub(string.lower(text), 1, 12) == "/addmexspawn" then
+hook.Add("PlayerSay", "NationalityCommands" , function( ply, text )
+
+	if string.sub( string.lower( text ), 1, 12 ) == "/addmexspawn" then
 		if ply:IsSuperAdmin() then
-			addMexSpawn(ply)
-			ply:ChatPrint("A Mexican spawn has been added for the map "..game.GetMap().."!")
+			addMexSpawn( ply )
+			ply:ChatPrint("A Mexican spawn has been added for the map " .. game.GetMap() .. "!")
 		else
 			ply:ChatPrint("You do not have permission to perform this action.")
 		end
         return ""
-	end
-	if string.sub(string.lower(text), 1, 11) == "/addamspawn" then
+    end
+    
+	if string.sub( string.lower( text ), 1, 11 ) == "/addamspawn" then
 		if ply:IsSuperAdmin() then
-			addAmSpawn(ply)
-			ply:ChatPrint("An American spawn has been added for the map "..game.GetMap().."!")
+			addAmSpawn( ply )
+			ply:ChatPrint("An American spawn has been added for the map " .. game.GetMap() .. "!")
 		else
 			ply:ChatPrint("You do not have permission to perform this action.")
 		end
         return ""
-	end
-	if string.sub(string.lower(text), 1, 14) == "/savenatspawns" then
+    end
+    
+	if string.sub( string.lower( text ), 1, 14 ) == "/savenatspawns" then
 		if ply:IsSuperAdmin() then
 			saveSpawns()
-			ply:ChatPrint("You have successfully saved spawns for the map "..game.GetMap().."!")
+			ply:ChatPrint("You have successfully saved spawns for the map " .. game.GetMap() .. "!")
 		else
 			ply:ChatPrint("You do not have permission to perform this action.")
 		end
         return ""
-	end
+    end
+    
+	if string.sub( string.lower( text ), 1, 14 ) == "/deletenatspawns" then
+		if ply:IsSuperAdmin() then
+			deleteSpawns( ply )
+		else
+			ply:ChatPrint("You do not have permission to perform this action.")
+		end
+        return ""
+    end
+    
 end)
 
