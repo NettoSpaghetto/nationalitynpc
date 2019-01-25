7ebd03b240f722979bdf5c92ee55222ae6d393fe
BlayneWright <41925552+NettoSpaghetto@users.noreply.github.com>
2019-01-25 20:43:10 +0000

    took out code not required

--- lua/nationality/sv/nationality_playerspawn.lua
+++ lua/nationality/sv/nationality_playerspawn.lua
@@ -32,9 +32,9 @@ local function nationalitySpawn(ply)
     if file.Exists( userDataFile, "DATA" ) then
         if ply:IsValid() and ply:IsPlayer() then
             if tonumber(userDataContents, 2)  == NATION_MEX then
-                timer.Simple(0.5, function() ply:SetPos( mexicanSpawn[math.random(1, table.Count( mexicanSpawn ) ) ] ) end )
+                timer.Simple(0.5, function() ply:SetPos( mexicanSpawn[math.random( table.Count( mexicanSpawn ) ) ] ) end )
             else
-                timer.Simple(0.5, function() ply:SetPos( americanSpawn[math.random(1, table.Count( americanSpawn ) ) ] ) end )
+                timer.Simple(0.5, function() ply:SetPos( americanSpawn[math.random( table.Count( americanSpawn ) ) ] ) end )
             end
         end
     end
